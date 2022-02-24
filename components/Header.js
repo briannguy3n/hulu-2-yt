8eef59b8836aec189837c533fdf4b6d6ff0b087d
Brian Nguyen <nguyenbrian604@gmail.com>
2022-02-23 19:52:58 -0800

    commit

--- components/Header.js
+++ components/Header.js
@@ -8,12 +8,18 @@ import {
   SearchIcon,
   UserIcon,
 } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
 
 function Header() {
+  const router = useRouter();
   return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
-        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem
+          title="HOME"
+          Icon={HomeIcon}
+          onClick={() => router.push(`/`)}
+        />
         <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
         <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
         <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
@@ -21,7 +27,8 @@ function Header() {
         <HeaderItem title="ACCOUNT" Icon={UserIcon} />
       </div>
       <Image
-        className="object-contain"
+        onClick={() => router.push(`/`)}
+        className="object-contain cursor-pointer"
         src="https://links.papareact.com/ua6"
         width={200}
         height={100}
