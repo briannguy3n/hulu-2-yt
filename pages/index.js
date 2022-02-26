f5d64a1b164c7f0518415a760150684558d0fbab
Brian Nguyen <nguyenbrian604@gmail.com>
2022-02-25 18:41:55 -0800

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
 import styles from "../styles/Home.module.css";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,11 +13,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
-      {/* Nav */}
       <Nav />
-      {/* Results */}
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
